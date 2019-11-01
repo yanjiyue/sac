8e77d881b47f2b0d2bb929b2b68c8b48ea25bb4e
yanjiyue <55572900+yanjiyue@users.noreply.github.com>
2019-11-01 20:17:37 +0800

    Update heap sort.py

--- 作業/hw2/heap sort.py
+++ 作業/hw2/heap sort.py
@@ -1,19 +1,3 @@
-
- def good(arr,start,end):
-   root=start
-   child=start+1
-   while child<=end:
-   #僅在子節點的位置在arr範圍之內時進行
-       if child+1<=end and arr[child+1]<arr[child]:
-        #若子節點的下一位小於子節點則互換；使root始終與較小值比較
-           child+=1
-       if arr[root]>arr[child]:
-        #若root大於子節點，則互換位置與坐標，以保證root始終為最小值
-           arr[root],arr[child]=arr[child],arr[root]
-           root=child
-           child=root+1
-       else:break
-
  class Solution(object):       
     def heap_sort(self,arr):
       end=len(arr)-1
@@ -23,3 +7,18 @@
         arr[0],arr[end]=arr[end],arr[0]#將末尾值與根互換，使最末值成為新根
         end-=1
       return arr
+   
+    def good(arr,start,end):
+      root=start
+      child=start+1
+      while child<=end:
+   #僅在子節點的位置在arr範圍之內時進行
+        if child+1<=end and arr[child+1]<arr[child]:
+        #若子節點的下一位小於子節點則互換；使root始終與較小值比較
+           child+=1
+        if arr[root]>arr[child]:
+        #若root大於子節點，則互換位置與坐標，以保證root始終為最小值
+           arr[root],arr[child]=arr[child],arr[root]
+           root=child
+           child=root+1
+        else:break
