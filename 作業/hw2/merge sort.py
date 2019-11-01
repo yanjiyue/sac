5f368ffec953a856a2025ac8d38b9371c2804445
yanjiyue <55572900+yanjiyue@users.noreply.github.com>
2019-11-01 20:20:42 +0800

    Create merge sort.py

---
+++ 作業/hw2/merge sort.py
@@ -0,0 +1,24 @@
+class Solution(object):
+  def mergesort(self,list):
+    if len(list)<=1:
+        return list
+    n=int(len(list)/2)#分為左右臨近的兩塊
+    left=list[:n]
+    right=list[n:]
+    return merge(mergesort(left),mergesort(right))#分別再對左右兩塊進行各自的左右分塊
+
+
+  def merge(left,right):
+    r,l=0,0
+    result=[]
+    while l<len(left) and r<len(right):#left中的數分別與right中的數進行比較、插入
+        if left[l]<=right[r]:#由於list內部已是比過大小的，所以若left第一個值小於right第一個值，則必定小於right中其他值，則無需再比較
+            result.append(left[l])
+            l+=1
+        else:
+            result.append(right[r])
+            r+=1
+    result+=list(left[l:])
+    result+=list(right[r:])#將剩餘的數加回結果list後，防止值的丟失
+    
+    return result
